4adbdbbcb4a32757ad36c169228555a8190e4ee4
jalal-elzein <jalal13elzein@gmail.com>
2024-05-12 05:40:27 +0300

    arg

--- Preprocessing/build.sh
+++ Preprocessing/build.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
+default_output_zip_filename="preprocessing.zip"
+
+if [ $# -eq 0 ]; then
+    output_zip_filename="$default_output_zip_filename"
+else
+    output_zip_filename="$1"
+fi
+
 mkdir preproc_package
 
 cp lambda_function.py preproc_package/lambda_function.py
 
 pip install pedalboard -t ./preproc_package
 
-cd preproc_package && zip -r ../preprocessing.zip .
+cd preproc_package && zip -r "../$output_zip_filename" .
+
+cd ..
+rm -rf preproc_package
