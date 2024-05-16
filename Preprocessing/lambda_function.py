254145b879416b538f49f75bd1026f712a0cdf7a
AhmadHLB <44374418+AhmadHLB@users.noreply.github.com>
2024-05-16 16:02:38 +0300

    fix preprocessing

--- Preprocessing/lambda_function.py
+++ Preprocessing/lambda_function.py
@@ -16,9 +16,8 @@ OUTPUT_BUCKET = os.environ.get("OUTPUT_BUCKET", "audio-processed-1")
 def convert_to_mp3(input_file, output_file):
     # Load the audio file
     audio = AudioSegment.from_file(input_file)
-
-    # Export the audio to MP3 format
-    audio.export(output_file, format="mp3")
+    
+    audio.export(output_file_mp3, format="mp3", codec="libmp3lame")
 
 
 def download_audio_file(bucket_name, key, audio_file_path):
